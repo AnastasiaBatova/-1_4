bbccc2c9a38a5cb23cd3e0bbc419611415c005a4
AnastasiaBatova <batosha9913@icloud.com>
2024-12-01 22:42:19 +0300

    Add files via upload

---
+++ modul_1_4.py
@@ -0,0 +1,7 @@
+my_string=input('От куда ты? ')
+print(len(my_string))
+print('от куда ты?' .upper())
+print('от куда ты?' .lower())
+print('от куда ты?' .replace(' ',''))
+print(my_string[0])
+print(my_string[-1])
